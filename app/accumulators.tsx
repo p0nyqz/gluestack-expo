691555838ebeeab18e062f130ad15eff2789c6c1
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2024-07-11 23:47:47 +0200

    feat: accumulators

--- app/accumulators.tsx
+++ app/accumulators.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'expo-router'
-import { View, SafeAreaView } from 'react-native'
+import { View, SafeAreaView, ScrollView } from 'react-native'
 import { Text, Heading, Box, VStack } from '@gluestack-ui/themed'
 
 import Deductible from '../components/Deductible'
@@ -7,47 +7,91 @@ import Deductible from '../components/Deductible'
 export default function Accumulators() {
   return (
     <Box flex={1} backgroundColor="#F3F2F8">
-      <SafeAreaView flexDirection="row">
-        <Link href="/">Go to Home Page</Link>
-        <Heading pb="$6">Accumulators</Heading>
-      </SafeAreaView>
+      <ScrollView
+        style={{ height: '100%' }}
+        contentContainerStyle={{ flexGrow: 1 }}
+      >
+        <Box $base-my="$16" $base-mx="$5">
+          <SafeAreaView flexDirection="row">
+            <Link href="/">Go to Home Page</Link>
+            <Heading pb="$6">Accumulators</Heading>
+          </SafeAreaView>
 
-      <VStack space="sm" reversed={false}>
-        <Box flexDirection="row" justifyContent="space-between">
-          <Text color="$warmGray400" size="sm">
-            Family
-          </Text>
+          {/* Family */}
+          <VStack space="sm" reversed={false}>
+            <Box flexDirection="row" justifyContent="space-between">
+              <Text color="$warmGray400" size="sm">
+                Family
+              </Text>
+            </Box>
+            <Box
+              $base-flexDirection="column"
+              $md-flexDirection="row"
+              bg="white"
+              px="$8"
+              py="$7"
+              borderRadius="16"
+            >
+              <Deductible
+                title="Medical Deductible"
+                amount1="$1650"
+                amount2="$3000"
+              />
+
+              <Deductible title="In Network" amount1="$2300" amount2="$7500" />
+
+              <Deductible
+                title="Out of Network"
+                amount1="$1230"
+                amount2="$38000"
+              />
+            </Box>
+          </VStack>
+
+          {/* Individual */}
+          {/* Family */}
+          <VStack space="sm" reversed={false}>
+            <Box flexDirection="row" justifyContent="space-between">
+              <Text
+                color="$warmGray600"
+                size="sm"
+                mt="$2"
+                fontWeight="$semibold"
+              >
+                John Smith
+              </Text>
+            </Box>
+            <Box
+              $base-flexDirection="column"
+              $md-flexDirection="row"
+              bg="white"
+              px="$8"
+              py="$7"
+              borderRadius="16"
+            >
+              <Deductible
+                title="Medical Deductible"
+                amount1="$1"
+                amount2="$750"
+              />
+
+              <Deductible
+                title="Dental Deductible"
+                amount1="$0"
+                amount2="$1000"
+              />
+
+              <Deductible title="In Network" amount1="$43" amount2="$3000" />
+
+              <Deductible
+                title="Out of Network"
+                amount1="$1230"
+                amount2="$38000"
+              />
+            </Box>
+          </VStack>
         </Box>
-        <Box
-          $base-flexDirection="column"
-          $md-flexDirection="row"
-          bg="white"
-          px="$8"
-          py="$7"
-          borderRadius="16"
-        >
-          <Deductible
-            title="Medical Deductible"
-            progressValue={60}
-            amount1="$1650"
-            amount2="$3000"
-          />
-
-          <Deductible
-            title="In Network"
-            progressValue={40}
-            amount1="$2300"
-            amount2="$7500"
-          />
-
-          <Deductible
-            title="Out of Network"
-            progressValue={10}
-            amount1="$1230"
-            amount2="$38000"
-          />
-        </Box>
-      </VStack>
+      </ScrollView>
     </Box>
   )
 }
