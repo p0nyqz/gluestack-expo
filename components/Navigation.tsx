2bdd9873fb0a18215ee3c96edf0772564a640b94
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2024-07-16 17:33:42 +0200

    feat: navigation styles, icons, routing

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -6,25 +6,53 @@ import {
   ProgressFilledTrack,
 } from '@gluestack-ui/themed'
 
+import DocumentData from '../assets/Icons/DocumentData'
+import Rocket from '../assets/Icons/Rocket'
+import LightBulbPerson from '../assets/Icons/LightbulbPerson'
+
+import { Link } from 'expo-router'
+import { Svg, Path } from 'react-native-svg'
+
 export default function Navigation() {
   return (
     <Box
-      position="fixed"
+      position="absolute"
       bottom={0}
       left={0}
       right={0}
       // height="70px"
-      minHeight="60px"
-      bg="white"
-      pb="$8"
-      borderRadius="$16"
+      // minHeight="60px"
+      minWidth="50px"
+      // bg="white"
+      p="$5"
+      px="$6"
+      mb="$8"
+      mx="$6"
+      borderRadius="$20"
       // shadow="$2"
       display="flex"
+      flexDirection="row"
       alignItems="center"
-      justifyContent="center"
-      bgColor="transparent"
+      justifyContent="space-between"
+      bgColor="#08122D"
+      boxShadow="0px -4px 8px rgba(0, 0, 0, 0.1)"
     >
-      <Text>Navigation</Text>
+      {/* <Text>Navigation</Text> */}
+      <Link href="/">
+        <DocumentData />
+      </Link>
+
+      <Link href="/cards">
+        <Rocket />
+      </Link>
+
+      <Link href="/claims">
+        <LightBulbPerson />
+      </Link>
+
+      <Link href="/profile">
+        <DocumentData />
+      </Link>
     </Box>
   )
 }
